0b0350cf21fbd0215bb822916a6a969fe9995248
Robert King <raking45@hotmail.com>
2024-11-03 21:07:37 -0500

    Week 2 Completed

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,30 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:  Robert King
+ * Date:    November 3, 2024
+ * File Name:   index.js
+ * Description:  Import functions from recipes.js using require, declares ingredients as an array, and
+ * implements command-line interface to demonstrate functionality.  
 */
 
 // TODO: Import your module using require
+const { createRecipe, setTimer, quit } = require("./recipes.js");
+
+// Array of Ingredients
+const ingredients = [
+    "flour",
+    "sugar",
+    "butter",
+    "egg",
+    "potato",
+    "bacon",
+    "onion",
+    "cheese",
+    "salt",
+    "pepper",
+    "ham"
+]
 
 // TODO: Implement your CLI program here
+console.log(createRecipe(ingredients));
+console.log(setTimer(10));
+console.log(quit());
